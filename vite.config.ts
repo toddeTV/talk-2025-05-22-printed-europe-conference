8404259837901011004e21e24a39cf7261b5936d
Thorsten Seyschab <business@todde.tv>
2025-05-20 22:15:16 +0200

    feat: add dependency `@todde.tv/gltf-type-toolkit`

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import gltf from '@todde.tv/gltf-type-toolkit/vite'
 import { templateCompilerOptions } from '@tresjs/core'
 import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
@@ -5,6 +6,7 @@ import svgLoader from 'vite-svg-loader'
 export default defineConfig({
   plugins: [
     svgLoader(),
+    gltf(),
   ],
   slidev: {
     // markdown: {
